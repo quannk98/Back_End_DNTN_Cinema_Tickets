c8274576b6849cd411b22b972f4bfa564de746e4
gitthuynv <contacts.devs@gmail.com>
2024-07-08 16:03:12 +0700

    fix actor

--- src/modules/actor/actor.module.ts
+++ src/modules/actor/actor.module.ts
@@ -4,6 +4,7 @@ import { Actor, ActorSchema } from 'src/schemas/actor.schema';
 import { ActorService } from './actor.service';
 import { ActorReponsitory } from 'src/repositories/actor.reponsitory';
 import { ActorController } from './actor.controller';
+import { ActorDto } from './dto/actor.dto';
 
 @Module({
     imports:[
